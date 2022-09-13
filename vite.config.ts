cd1252eb1c1e131f9aa20f71342ffb7d5ffb18b3
Diego Assencio <diego@assencio.com>
2022-09-13 09:55:54 +0000

    Add global SCSS file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/global.scss";`,
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
