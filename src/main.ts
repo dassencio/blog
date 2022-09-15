024600112096be8d01b52ad3e44992cbc5765250
Diego Assencio <diego@assencio.com>
2022-09-15 16:39:21 +0000

    Make BlockEquation available globally

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import "ress";
 import App from "./App.vue";
+import BlockEquation from "./components/BlockEquation.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+app.component("BlockEquation", BlockEquation);
 
 app.use(router);
 
