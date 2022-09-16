9986c9ecb19c24d56d2a8a1a9fc1201afbb991b0
Diego Assencio <diego@assencio.com>
2022-09-16 11:06:30 +0000

    Move CSS styles to a better place

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,3 @@
 import { RouterView } from "vue-router";
 import NavBar from "@/components/NavBar.vue";
 </script>
-
-<style lang="scss">
-@font-face {
-  font-family: "Open Sans Regular";
-  src: url("@/assets/fonts/opensans-regular.ttf") format("truetype");
-}
-@font-face {
-  font-family: "Open Sans Light";
-  src: url("@/assets/fonts/opensans-light.ttf") format("truetype");
-}
-* {
-  font-family: "Open Sans Regular";
-  font-weight: normal;
-  line-height: 1;
-}
-html {
-  background-color: white;
-  height: 100%;
-}
-</style>
