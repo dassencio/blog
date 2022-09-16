c4fd836517a879ddb4d5c2d9b715dd8ccca7f63c
Diego Assencio <diego@assencio.com>
2022-09-16 14:20:07 +0000

    Add styles for post text

--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,26 @@ $base-length: $view-paragraph-font-size;
   max-width: 100%;
   padding: (3.5 * $base-length) $base-length;
   width: $desktop-width-mode;
+  :deep(p) {
+    line-height: $view-paragraph-line-height;
+    margin: $base-length 0;
+    text-align: left;
+    @media (min-width: $desktop-width-mode) {
+      text-align: justify;
+    }
+  }
+  :deep(a),
+  :deep(mjx-container a > g) {
+    color: $view-link-color-normal;
+    fill: $view-link-color-normal;
+    text-decoration: none;
+    &:active,
+    &:focus,
+    &:hover {
+      color: $view-link-color-active;
+      fill: $view-link-color-active;
+      text-decoration: underline;
+    }
+  }
 }
 </style>
