f7259b7edf9b6421126f125f68cb80a9e30b9996
Diego Assencio <diego@assencio.com>
2022-09-17 13:06:19 +0000

    Document TS code using TSDoc standard

--- src/functions.ts
+++ src/functions.ts
@@ -1,9 +1,10 @@
 /**
- * Convert a date into a string in the format "YYYY-MM-DD".
+ * Converts a date into its representation in the ISO 8601 format.
  *
- * @param year Full year as an integer value.
- * @param month Month as an integer value (1-12).
- * @param day Day as as an integer value (1-31).
+ * @param year - Full year as an integer value
+ * @param month - Month as an integer value (1-12)
+ * @param day - Day as as an integer value (1-31)
+ * @returns Date in format "YYYY-MM-DD"
  */
 export function dateToString(year: number, month: number, day: number): string {
   const date = new Date(Date.UTC(year, month - 1, day));
