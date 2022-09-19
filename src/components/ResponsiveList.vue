c2f94817ca13ab5ba03986374f2b1945d332fbd1
Diego Assencio <diego@assencio.com>
2022-09-19 20:18:31 +0000

    Add component for displaying responsive lists

---
+++ src/components/ResponsiveList.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="list" :style="style">
+    <slot></slot>
+  </p>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+const props = defineProps<{
+  itemized?: boolean;
+}>();
+
+const style = computed(() => {
+  return {
+    gridTemplateColumns: props.itemized ? "auto minmax(0, 1fr)" : "1fr",
+  };
+});
+</script>
+
+<style scoped lang="scss">
+.list {
+  display: grid;
+  gap: $view-responsive-list-vertical-gap $view-responsive-list-horizontal-gap;
+  margin-left: $view-indentation-length;
+  :deep(> *) {
+    line-height: $view-paragraph-line-height;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from "vue";
 import "ress";
 import App from "@/App.vue";
 import BlockEquation from "@/components/BlockEquation.vue";
+import ResponsiveList from "@/components/ResponsiveList.vue";
 import router from "@/router";
 
 const app = createApp(App);
 
 app.component("BlockEquation", BlockEquation);
+app.component("ResponsiveList", ResponsiveList);
 
 app.use(router);
 
