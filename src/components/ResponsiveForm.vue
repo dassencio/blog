83eb367ea1dc9532c3cbfbf1c95dc5dd01596f1d
Diego Assencio <diego@assencio.com>
2022-09-25 14:36:43 +0000

    Add component for displaying forms

---
+++ src/components/ResponsiveForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="form" form>
+    <span class="form__contents">
+      <slot></slot>
+    </span>
+  </p>
+</template>
+
+<style scoped lang="scss">
+.form {
+  padding: $view-vertical-correction-padding 0;
+  text-align: center;
+  &__contents {
+    display: inline-flex;
+    flex-direction: column;
+    gap: $view-form-vertical-gap;
+    max-width: 100%;
+  }
+}
+</style>
