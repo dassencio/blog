712037eac263f6e7657c32d5526c1a9c74af17b1
Diego Assencio <diego@assencio.com>
2022-09-25 14:37:16 +0000

    Make components available globally

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { createApp } from "vue";
 import "ress";
 import App from "@/App.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import BinaryOption from "@/components/BinaryOption.vue";
 import BlockEquation from "@/components/BlockEquation.vue";
+import InputField from "@/components/InputField.vue";
 import MultiModeGrid from "@/components/MultiModeGrid.vue";
+import ResponsiveForm from "@/components/ResponsiveForm.vue";
 import router from "@/router";
 
 const app = createApp(App);
 
+app.component("BaseButton", BaseButton);
+app.component("BinaryOption", BinaryOption);
 app.component("BlockEquation", BlockEquation);
+app.component("InputField", InputField);
 app.component("MultiModeGrid", MultiModeGrid);
+app.component("ResponsiveForm", ResponsiveForm);
 
 app.use(router);
 
