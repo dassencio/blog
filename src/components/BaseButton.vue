dd31b14faa786b7f344e8ca7e2c7e94b2ce2ddf7
Diego Assencio <diego@assencio.com>
2022-09-25 13:45:52 +0000

    Add form element: button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    class="button"
+    :class="buttonClass"
+    @keydown.enter="onKeyDown"
+    @keydown.space="onKeyDown"
+    @keyup.enter="onKeyUp"
+    @keyup.space="onKeyUp"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const buttonClass = ref("");
+const emit = defineEmits(["clicked"]);
+
+function onKeyDown() {
+  buttonClass.value = "active";
+}
+function onKeyUp() {
+  buttonClass.value = "";
+  emit("clicked");
+}
+</script>
+
+<style scoped lang="scss">
+.button {
+  background: $view-button-background-color-normal;
+  border-radius: $view-form-element-border-radius;
+  border: $view-form-element-border-normal;
+  outline: none;
+  padding: $view-button-vertical-padding $view-button-horizontal-padding;
+  text-align: center;
+  user-select: none;
+  white-space: nowrap;
+  &.active,
+  &:active {
+    border: $view-form-element-border-active;
+  }
+  &:focus,
+  &:hover {
+    background: $view-button-background-color-focus;
+    &:not(:active):not(.active) {
+      border: $view-form-element-border-focus;
+    }
+  }
+}
+</style>
