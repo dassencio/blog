5e203cc8916372457303b7ca539028864460382d
Diego Assencio <diego@assencio.com>
2022-10-25 21:57:21 +0000

    Add component for displaying subfigures

---
+++ src/components/BaseSubfigure.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="subfigure">
+    <slot></slot>
+    <span class="subfigure__label">{{ label }}</span>
+  </span>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.subfigure {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  row-gap: $view-subfigure-vertical-gap;
+  &__label {
+    line-height: 1;
+  }
+}
+</style>
