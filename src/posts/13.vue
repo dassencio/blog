2175e66d19de147ae8168d723e77760231561456
Diego Assencio <diego@assencio.com>
2022-11-01 08:36:17 +0000

    Improve post text

--- src/posts/13.vue
+++ src/posts/13.vue
@@ -5,13 +5,13 @@
   </p>
 
   <CodeBlock
-    >sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap
-  </CodeBlock>
+    >sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap</CodeBlock
+  >
 
   <p>
     This will enable all users to capture live traffic going through any network
-    interface. If this is what you wish, you're done: just run
-    <code>wireshark</code> and have fun!
+    interface. If this is what you wish, you're done: just start Wireshark and
+    have fun!
   </p>
 
   <p>
@@ -33,44 +33,40 @@
   <p>
     If you feel that granting this privilege to all users is too permissive,
     then you should create a group and only allow users within that group to
-    execute <code>dumpcap</code> (and therefore to capture live traffic with
-    Wireshark). On Ubuntu/Debian, you can create a group called
+    execute Dumpcap (and therefore to capture live traffic with Wireshark). On
+    Ubuntu/Debian, you can create a group called
     <code>wireshark</code> with the following command:
   </p>
 
   <CodeBlock>sudo addgroup wireshark</CodeBlock>
 
-  <p>
-    Now change the group of the <code>dumpcap</code> binary to
-    <code>wireshark</code>:
-  </p>
+  <p>Now change the group of Dumpcap to <code>wireshark</code>:</p>
 
   <CodeBlock>sudo chgrp wireshark /usr/bin/dumpcap</CodeBlock>
 
   <p>
     Next, make sure only users in the <code>wireshark</code> group can execute
-    <code>dumpcap</code>:
+    Dumpcap:
   </p>
 
   <CodeBlock>sudo chmod 754 /usr/bin/dumpcap</CodeBlock>
 
   <p>
-    Finally, add the necessary capabilities to <code>dumpcap</code> as discussed
-    above (even if you already ran this command before, run it again as
-    <code>dumpcap</code> might have lost its given capabilities at this point):
+    Finally, add the necessary capabilities to Dumpcap as discussed above (even
+    if you already ran this command before, run it again as Dumpcap might have
+    lost its given capabilities at this point):
   </p>
 
   <CodeBlock
-    >sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap
-  </CodeBlock>
+    >sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap</CodeBlock
+  >
 
   <p>
-    To test if your permissions are correct, run
-    <code>wireshark</code> as a regular user (I'm assuming you have not added
-    this user to the <code>wireshark</code> group). If you cannot capture live
-    traffic, everything is fine. At this point, you add can users who should be
-    allowed to capture live traffic to the <code>wireshark</code> group with the
-    command below:
+    To check if your permissions are correct, start Wireshark as a regular user
+    (I'm assuming you haven't added this user to the
+    <code>wireshark</code> group yet). If you cannot capture live traffic,
+    everything is fine. At this point, you can add users to the
+    <code>wireshark</code> group with the command below:
   </p>
 
   <CodeBlock>sudo usermod -a -G wireshark &lt;username&gt;</CodeBlock>
@@ -88,11 +84,12 @@
   </p>
 
   <CodeBlock
-    >uid=1000(username) gid=1000(username) ..., 1001(<b>wireshark</b>)
-  </CodeBlock>
+    >uid=1000(username) gid=1000(username) ...,
+    1001(<b>wireshark</b>)</CodeBlock
+  >
 
   <p>
-    Now run <code>wireshark</code> as one of the users in the
+    Now start Wireshark as one of the users in the
     <code>wireshark</code> group: you should be able to capture live traffic. If
     you did all of the above while logged in as one of the users you added to
     the <code>wireshark</code> group, you may need to log out and log back in
@@ -100,10 +97,10 @@
   </p>
 
   <p>
-    <b>NOTE</b>: While in principle you could simply run
-    <code>wireshark</code> as root instead of following the instructions above,
-    <b>do not do this</b>! Wireshark is a huge application, and running it as
-    root unnecessarily risks damaging your system.
+    <b>NOTE</b>: While in principle you could simply run Wireshark as root
+    instead of following the instructions above, <b>do not do this</b>!
+    Wireshark is a huge application, and running it as root unnecessarily risks
+    damaging your system.
   </p>
 
   <p>
