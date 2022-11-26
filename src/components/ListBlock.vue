8c24e06aed1c063744a688e16f3397ecfa4285e4
Diego Assencio <diego@assencio.com>
2022-11-26 19:18:53 +0100

    Add component for displaying lists

---
+++ src/components/ListBlock.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="list">
+    <slot></slot>
+  </p>
+</template>
+
+<style scoped lang="scss">
+.list {
+  display: grid;
+  gap: $view-list-block-vertical-gap $view-list-block-horizontal-gap;
+  grid-template-columns: auto minmax(0, 1fr);
+  margin-left: $view-list-block-horizontal-gap;
+  :deep(> *) {
+    line-height: $view-paragraph-line-height;
+  }
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="item"><span class="item__bullet"></span></span><slot></slot>
+</template>
+
+<style scoped lang="scss">
+.item {
+  align-items: center;
+  display: flex;
+  height: $view-paragraph-line-height;
+  justify-self: center;
+  &__bullet {
+    background-color: $view-text-color-normal;
+    height: $view-list-bullet-size;
+    width: $view-list-bullet-size;
+  }
+}
+</style>
