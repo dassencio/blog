beff284763d830d67a2b3e911b0baafbe6226b29
Diego Assencio <diego@assencio.com>
2022-12-02 19:42:22 +0100

    Use div as wrapper element for tables

--- src/components/TableBlock.vue
+++ src/components/TableBlock.vue
@@ -1,12 +1,12 @@
 <template>
-  <p class="table">
-    <span
+  <div class="table">
+    <div
       class="table__contents"
       :style="{ gridTemplateColumns: `repeat(${columns}, auto)` }"
     >
       <slot></slot>
-    </span>
-  </p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,18 +17,15 @@ defineProps<{
 
 <style scoped lang="scss">
 .table {
+  line-height: $view-paragraph-line-height;
+  margin: $view-paragraph-vertical-margin 0;
   max-width: 100%;
   overflow-x: auto;
   overflow-y: hidden;
   text-align: center;
   &__contents {
     display: inline-grid;
-    :deep(> *) {
-      line-height: $view-paragraph-line-height;
-      padding: $view-table-block-vertical-padding
-        $view-table-block-horizontal-padding;
-      white-space: nowrap;
-    }
+    gap: $view-table-block-vertical-gap $view-table-block-horizontal-gap;
   }
 }
 </style>
