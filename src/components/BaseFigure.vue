b174049c396ac439a643d24109f8e55952ccc97c
Diego Assencio <diego@assencio.com>
2022-12-02 16:33:26 +0100

    Use div as wrapper element for figures

--- src/components/BaseFigure.vue
+++ src/components/BaseFigure.vue
@@ -1,11 +1,11 @@
 <template>
-  <p class="figure" :id="`fig_${number}`">
-    <span class="figure__contents"><slot></slot></span>
-    <span class="figure__caption">
+  <div class="figure" :id="`fig_${number}`">
+    <div class="figure__contents"><slot></slot></div>
+    <div class="figure__caption">
       <b class="figure__number">Fig. {{ number }}:</b>
-      <span class="figure__description">{{ description }}</span>
-    </span>
-  </p>
+      <div class="figure__description">{{ description }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,12 +17,14 @@ defineProps<{
 
 <style scoped lang="scss">
 .figure {
+  margin: $view-paragraph-vertical-margin 0;
   padding-top: $view-vertical-correction-padding;
   text-align: center;
   &__caption {
     column-gap: $view-figure-caption-horizontal-gap;
     display: flex;
     justify-content: center;
+    line-height: $view-paragraph-line-height;
     margin: $view-figure-caption-vertical-margin 0;
     padding: 0 $view-figure-caption-horizontal-padding;
   }
