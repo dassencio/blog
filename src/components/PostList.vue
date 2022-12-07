dassencio/blog

commit 8fbe19ef468e982c5f558416c1b016a60c434b9e

Simplify code

src/components/PostList.vue
@@ -30,9 +30,7 @@ const matchingPosts = computed(() => {
     : posts;
 });
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 </script>
 
 <style scoped lang="scss">

src/posts/1.vue
@@ -67,7 +67,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 </script>

src/posts/12.vue
@@ -149,9 +149,7 @@ import chargesOnRegularPolygon from "@/assets/images/charges-on-regular-polygon.
 import chargesOnRegularPolygonRotationA from "@/assets/images/charges-on-regular-polygon-rotation-a.svg";
 import chargesOnRegularPolygonRotationB from "@/assets/images/charges-on-regular-polygon-rotation-b.svg";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 </script>
 
 <style scoped lang="scss">

src/posts/15.vue
@@ -136,7 +136,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 </script>

src/posts/19.vue
@@ -308,9 +308,7 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 
 const N = 365;
 

src/posts/2.vue
@@ -157,7 +157,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 </script>

src/posts/3.vue
@@ -234,9 +234,7 @@ import _ from "lodash";
 import "lodash.combinations";
 import mathjax from "@/mathjax";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 
 const findAllSolutions = ref(false);
 const fugitiveTimes = ref("1, 2, 5, 10");

src/posts/4.vue
@@ -106,7 +106,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(() => {
-  mathjax.render();
-});
+onMounted(mathjax.render);
 </script>

commit ad1d0eeaaa0800bea7bef961949384f15a231f08

Fix rendering of mathematics text
Commit 8fbe19ef468e982c5f558416c1b016a60c434b9e caused onMounted to
possibly register the initial version of mathjax.render(), which is a
no-op until asynchronously redefined by MathJax. This commit fixes that.

src/components/PostList.vue
@@ -30,7 +30,7 @@ const matchingPosts = computed(() => {
     : posts;
 });
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>
 
 <style scoped lang="scss">

src/posts/1.vue
@@ -67,5 +67,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>

src/posts/12.vue
@@ -149,7 +149,7 @@ import chargesOnRegularPolygon from "@/assets/images/charges-on-regular-polygon.
 import chargesOnRegularPolygonRotationA from "@/assets/images/charges-on-regular-polygon-rotation-a.svg";
 import chargesOnRegularPolygonRotationB from "@/assets/images/charges-on-regular-polygon-rotation-b.svg";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>
 
 <style scoped lang="scss">

src/posts/15.vue
@@ -136,5 +136,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>

src/posts/19.vue
@@ -308,7 +308,7 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 
 const N = 365;
 

src/posts/2.vue
@@ -157,5 +157,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>

src/posts/3.vue
@@ -234,7 +234,7 @@ import _ from "lodash";
 import "lodash.combinations";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 
 const findAllSolutions = ref(false);
 const fugitiveTimes = ref("1, 2, 5, 10");

src/posts/4.vue
@@ -106,5 +106,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>

src/posts/6.vue
@@ -85,5 +85,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>

src/posts/7.vue
@@ -156,5 +156,5 @@
 import { onMounted } from "vue";
 import mathjax from "@/mathjax";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>

src/posts/8.vue
@@ -200,7 +200,7 @@ import mathjax from "@/mathjax";
 import circularMotion from "@/assets/images/circular-motion.svg";
 import circularMotionAV from "@/assets/images/circular-motion-a-v.svg";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>
 
 <style scoped lang="scss">

src/posts/9.vue
@@ -177,7 +177,7 @@ import carTravelingAlongArc from "@/assets/images/car-traveling-along-arc.svg";
 import circularMotion from "@/assets/images/circular-motion.svg";
 import washingMachineForces from "@/assets/images/washing-machine-forces.svg";
 
-onMounted(mathjax.render);
+onMounted(() => mathjax.render());
 </script>
 
 <style scoped lang="scss">