8592c0814a40635523c689f05cc017395b27ccb5
Diego Assencio <diego@assencio.com>
2022-12-19 15:42:25 +0100

    Improve post text

--- src/posts/20.vue
+++ src/posts/20.vue
@@ -8,11 +8,11 @@
   </p>
 
   <p>
-    Since each commit must have a unique hash, one question that naturally
-    arises is:
+    Since each commit must have a unique hash, a question which naturally arises
+    is:
     <i>
-      How likely is it that the hash generated for a future commit will coincide
-      with the hash of some past commit?
+      How likely is it that the hash generated for a future commit will be the
+      same as the hash of some past commit?
     </i>
   </p>
 
@@ -20,12 +20,12 @@
     This question is closely related to the
     <a href="https://en.wikipedia.org/wiki/Birthday_problem">birthday paradox</a
     >. In a <RouterLink :to="{ name: '19' }">previous post</RouterLink>, I have
-    discussed the birthday paradox in detail and showed that when we randomly
-    select $n$ elements from a
+    analyzed the birthday paradox in detail and showed that when we randomly
+    select elements from a
     <a href="https://en.wikipedia.org/wiki/Set_%28mathematics%29">set</a>
-    containing $N$ elements (a set contains only distinct elements), the
-    probability of obtaining the same element more than once will be greater
-    than $50\%$ if $n \geq 1.2\sqrt{N}$ provided that $\sqrt{N} \gg 1$.
+    containing $N$ distinct elements $n$ times, the probability of obtaining the
+    same element more than once will be approximately $50\%$ if $n \approx
+    1.2\sqrt{N}$ provided that $\sqrt{N} \gg 1$.
   </p>
 
   <p>
@@ -34,20 +34,18 @@
   </p>
 
   <EquationBlock>
-    N_{\textrm{SHA-1}} = 2^{160} \approx 1.46 \cdot 10^{48}
+    N_{\textrm{SHA-1}} = 2^{160} \approx 1.5 \cdot 10^{48}
   </EquationBlock>
 
   <p>
     Given that $\sqrt{N} = 2^{80} \approx 1.2 \cdot 10^{24} \gg 1$, this means
-    that in order to obtain the same SHA-1 hash value for different inputs (in
-    technical words, to obtain a
-    <a href="https://en.wikipedia.org/wiki/Collision_(computer_science)"
-      >hash collision</a
-    >) with probability larger than $50\%$, we must generate approximately:
+    that in order to obtain the same SHA-1 hash value for different commits (a
+    <a href="https://en.wikipedia.org/wiki/Hash_collision">hash collision</a>)
+    with probability $50\%$, we must generate approximately:
   </p>
 
   <EquationBlock>
-    n \approx 1.2\sqrt{2^{160}} \approx 1.45 \cdot 10^{24}
+    n \approx 1.2\sqrt{N_{\textrm{SHA-1}}} \approx 1.5 \cdot 10^{24}
   </EquationBlock>
 
   <p>hashes.</p>
@@ -55,9 +53,11 @@
   <p>
     Assume that a given project contains $D$ active developers who generate
     $C_{\textrm{day}}$ commits per day. The number of commits
-    $C^T_{\textrm{year}}$ generated per year by such a group is (years with
-    $366$ days are ignored here, but the impact of this assumption on the end
-    result is negligible for our purposes):
+    $C^T_{\textrm{year}}$ generated per year by such a group is (<a
+      href="https://en.wikipedia.org/wiki/Leap_year"
+      >leap years</a
+    >
+    are not considered here, but their impact on the end result is negligible):
   </p>
 
   <EquationBlock> C^T_{\textrm{year}} = 365 D C_{\textrm{day}} </EquationBlock>
@@ -68,7 +68,7 @@
   </p>
 
   <EquationBlock>
-    Y = \frac{1.45 \cdot 10^{24}}{C^T_{\textrm{year}}} \Longrightarrow Y \approx
+    Y = \frac{1.5 \cdot 10^{24}}{C^T_{\textrm{year}}} \Longrightarrow Y \approx
     \frac{4 \cdot 10^{21}}{D C_{\textrm{day}}} \label{eq_y}
   </EquationBlock>
 
@@ -86,13 +86,13 @@
 
   <p>
     We would therefore need approximately $6.6$ million years to produce a
-    number of commits large enough to create a hash collision with $50\%$
+    number of commits large enough to obtain a hash collision with $50\%$
     probability!
   </p>
 
   <p>
     In a more realistic scenario, a large project might have $ 1000$ active
-    developers who commit $10$ commits per day. In these circumstances, it would
+    developers who commit $10$ commits per day. In those circumstances, it would
     take approximately $4 \cdot 10^{17}$ years for a collision to happen with
     $50\%$ probability. For comparison, the
     <a href="https://en.wikipedia.org/wiki/Age_of_the_universe"
@@ -103,14 +103,10 @@
 
   <p>
     To summarize, the probability of producing a hash collision on a Git
-    repository is so small that it will very likely not happen during our
+    repository is so small that it's extremely unlikely to happen during our
     lifetimes. In any case, if you're wondering what would happen to a
     repository in the event of a hash collision, you can find the answer in
-    <a
-      href="http://stackoverflow.com/questions/9392365/how-would-git-handle-a-sha-1-collision-on-a-blob"
-    >
-      this page</a
-    >.
+    <a href="https://stackoverflow.com/q/9392365">this page</a>.
   </p>
 </template>
 
