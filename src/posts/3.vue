52ac92b7b595897e1b1edd61193cc84a0e3d2531
Diego Assencio <diego@assencio.com>
2023-03-05 16:27:23 +0100

    Remove spurious space

--- src/posts/3.vue
+++ src/posts/3.vue
@@ -155,7 +155,7 @@
 
   <!-- prettier-ignore -->
   <EquationBlock align>
-    P_N &= \frac{N(N - 1)}{2} \cdot 2 \cdot \frac{(N - 1)(N - 2)}{2} 
+    P_N &= \frac{N(N - 1)}{2} \cdot 2 \cdot \frac{(N - 1)(N - 2)}{2}
            \ldots (N - 2) \frac{3 \cdot 2}{2} (N - 1) \frac{2 \cdot 1}{2} \\
         &= \left[2 \ldots (N - 2)(N - 1)\right] \left[\frac{N(N - 1)}{2}
            \frac{(N - 1)(N - 2)}{2} \ldots \frac{3 \cdot 2}{2}
