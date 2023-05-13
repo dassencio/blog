1701ca8e20642b44098e288ca9e4e97634fcc89f
Diego Assencio <diego@assencio.com>
2023-05-13 10:33:02 +0200

    Improve post text

--- src/posts/34.vue
+++ src/posts/34.vue
@@ -1,29 +1,29 @@
 <template>
   <p>
     In a <RouterLink :to="{ name: '30' }">previous post</RouterLink>, I
-    described how matrices can be compressed by using a tool in linear algebra
-    called
+    described how matrices can be compressed using a tool in linear algebra
+    known as
     <a href="https://en.wikipedia.org/wiki/Singular_value_decomposition">
       singular value decomposition</a
     >
     (SVD). The trick is to discard information (singular values) from the
     original matrix to generate an "approximate" version of it. This post
-    investigates whether that technique can also be used to compress images. It
-    should be noted that the method shown here is for educational purposes only
-    and is not suitable for professional image compression.
+    investigates whether this technique can also be used to compress images.
+    It's important to note that the method shown here is for educational
+    purposes only and is not suitable for professional image compression.
   </p>
 
   <p>
     I will use <a href="https://www.gnu.org/software/octave/">Octave</a> to
-    illustrate the method. If you're using Ubuntu/Debian, you can install it by
-    opening a terminal and running the following command (notice that the
+    demonstrate the method. If you're using Ubuntu/Debian, you can install it by
+    opening a terminal and running the following command (note that the
     <code>octave-image</code> package must also be installed):
   </p>
 
   <CodeBlock code="sudo apt-get install octave octave-image" />
 
   <p>
-    Now start Octave. Images can be read in Octave with the
+    Now, start Octave. Images can be read in Octave using the
     <code>imread</code> function:
   </p>
 
@@ -37,11 +37,11 @@
   </FigureBlock>
 
   <p>
-    The command above will read the file <code>dog-color.png</code> (shown in
-    <FigureLink id="dog-color" />) and store it as a three-dimensional array
+    The command above reads the file <code>dog-color.png</code> (shown in
+    <FigureLink id="dog-color" />) and stores it as a three-dimensional array
     $I$. The value $I_{ijn}$ is a one-byte integer (i.e., a value in the integer
     range $[0, 255]$) representing the amount of color $n$ on pixel $(i, j)$,
-    where $n = 1, 2, 3$ for the colors red, green, and blue respectively, and
+    where $n = 1, 2, 3$ for the colors red, green, and blue, respectively, and
     $(i, j)$ represents the position of a pixel in the image, with $i$ and $j$
     both equal to $1$ at the top-left corner of the image and increasing along
     the vertical and horizontal directions respectively. For the sample image in
@@ -51,7 +51,7 @@
   </p>
 
   <p>
-    Before doing image compression using $I$, let's first work on a simpler
+    Before performing image compression using $I$, let's first work on a simpler
     example: the grayscale version of the image in
     <FigureLink id="dog-color" />. Octave has a built-in function that converts
     color images to grayscale:
@@ -60,10 +60,10 @@
   <CodeBlock language="matlab" code="J = rgb2gray(I);" />
 
   <p>
-    This makes things easier since $J$ is simply a $400 \times 400$ matrix
+    This simplifies matters since $J$ is simply a $400 \times 400$ matrix
     containing the grayscale intensity of each pixel in the original image.
     Entries from $J$ are values in the range $[0, 255]$ with $0$ representing
-    black, $255$ representing white, and values in between representing
+    black, $255$ representing white, and intermediate values representing
     different shades of gray. To visualize $J$ in Octave, run:
   </p>
 
@@ -84,9 +84,8 @@
   <p>
     Now we can proceed to compress $J$. The function defined below will be used
     to compress matrices in the same way as described in
-    <RouterLink :to="{ name: '30' }">this post</RouterLink>, with the $N$
-    largest singular values from $J$ being preserved while all others are
-    discarded:
+    <RouterLink :to="{ name: '30' }">this post</RouterLink>. The $N$ largest
+    singular values from $J$ will be preserved while all others are discarded:
   </p>
 
   <CodeBlock
@@ -105,21 +104,21 @@
 
   <p>
     <b>NOTE</b>: The entries in $J$ are one-byte integers, but the entries in
-    $U_c$, $\Sigma_c$ and $V_c$ are
+    $U_c$, $\Sigma_c$, and $V_c$ are
     <a
       href="https://en.wikipedia.org/wiki/Double-precision_floating-point_format"
       >double-precision numbers</a
     >
-    (or "doubles", for short) which need eight bytes to be stored. This means
-    the total number of entries in these three matrices cannot be directly
+    (or "doubles", for short), which require eight bytes to be stored. This
+    means the total number of entries in these three matrices cannot be directly
     compared with the number of entries in $J$ for storage considerations since
-    they don't take up the same amount of space each. For the purposes of this
+    they don't occupy the same amount of space each. For the purposes of this
     post, however, this will not be an issue since our goal is to compress
     images, not matrices.
   </p>
 
   <p>
-    Let's go ahead and compress $J$ by discarding all but its $N = 50$ largest
+    Let's proceed to compress $J$ by retaining only its $N = 50$ largest
     singular values:
   </p>
 
@@ -133,17 +132,17 @@
 
   <p>
     The values of the matrix $J_c = U_c\Sigma_c V_c^T$ must first be converted
-    from doubles back to one-byte integers to correctly represent grayscale
+    from doubles back to one-byte integers to accurately represent grayscale
     intensities. The smaller the value of $N$, the less information from $J$
-    will be preserved, so discarding too many singular values will yield an
-    image with very poor quality. To visualize the image represented by $J_c$,
+    will be preserved. Therefore, discarding too many singular values will yield
+    an image of very poor quality. To visualize the image represented by $J_c$,
     run:
   </p>
 
   <CodeBlock language="matlab" code="imshow(Jc)" />
 
   <p>
-    To visualize both the original and generated images next to each other, run:
+    To visualize both the original and the generated images side by side, run:
   </p>
 
   <CodeBlock
@@ -169,10 +168,10 @@
   <FigureBlock
     id="dog-grayscale-compression"
     caption="Images generated by compressing $J$ with different values of $N$.
-             Figures (a), (b), (c), (d), (e) and (f) show the resulting images
-             for $N = 200$, $100$, $50$, $25$, $10$ and $5$ respectively. Notice
-             how the main aspects of the original grayscale image are preserved
-             even for small values of $N$."
+             Figures (a), (b), (c), (d), (e), and (f) show the resulting images
+             for $N = 200$, $100$, $50$, $25$, $10$, and $5$ respectively.
+             Observe how the main aspects of the original grayscale image are
+             preserved even for small values of $N$."
   >
     <BaseSubfigure>
       <ResponsiveImage
@@ -219,14 +218,13 @@
   </FigureBlock>
 
   <p>
-    A question which is worth asking at this point is: If we store the image
-    represented by $J_c$ as a file in PNG format, will it be smaller in size
-    than a file similarly generated from $J$? The answer is not a clear "yes"
-    since despite being a lossless format, PNG has its own compression mechanism
-    which is not the same as the one we used here and might therefore not be
-    able to benefit much from our "matrix compression" trick. To see what
-    happens, let's store both $J$ and $J_c$ as PNG images and check the
-    resulting file sizes:
+    It is worth asking at this point: If we store the image represented by $J_c$
+    as a file in PNG format, will it be smaller in size than a file similarly
+    generated from $J$? The answer is not a clear "yes" since, despite being a
+    lossless format, PNG has its own compression mechanism which is not the same
+    as the one we used here and may therefore not benefit much from our "matrix
+    compression" trick. To see what happens, let's store both $J$ and $J_c$ as
+    PNG images and check the resulting file sizes:
   </p>
 
   <CodeBlock
@@ -247,7 +245,7 @@
     id="image-sizes-grayscale"
     :columns="2"
     caption="Size of generated PNG images for different values
-             of $N$. Notice that $N = 400$ corresponds to the original image
+             of $N$. Observe that $N = 400$ corresponds to the original image
              since all singular values are kept during the compression process."
   >
     <b>$N$</b>
@@ -276,22 +274,22 @@
   </TableBlock>
 
   <p>
-    As we can see from the numbers above, the PNG images become smaller as we
-    discard more singular values. However, the reduction in size occurs very
-    slowly as $N$ is decreased. Even for $N = 5$ (see
+    Looking at the numbers above, we see that the PNG images gradually become
+    smaller as we discard more singular values. However, this reduction in size
+    unfolds very slowly as $N$ is decreased. Even for $N = 5$ (see
     <FigureLink id="dog-grayscale-compression" subfigure-id="N5" />), the
-    resulting image is only $46\%$ smaller than the original, despite looking
-    very blurred.
+    resulting image is only $46\%$ smaller than the original, despite appearing
+    significantly blurred.
   </p>
 
   <SectionTitle>Compressing the original color image</SectionTitle>
 
   <p>
-    We will now use the method described above on the original color image.
-    Remember that for a fixed $n$, $I_{ijn}$ is a $400 \times 400$ matrix which
-    contains the intensity of color $n$ at each pixel in the image, meaning we
-    can compress $I_{ij1}$, $I_{ij2}$ and $I_{ij3}$ separately. The following
-    code does exactly that for $N = 50$:
+    We will now apply the method described above to the original color image.
+    Recall that for a fixed $n$, $I_{ijn}$ is a $400 \times 400$ matrix that
+    contains the intensity of color $n$ at each pixel in the image. This implies
+    we can compress $I_{ij1}$, $I_{ij2}$, and $I_{ij3}$ separately. The
+    following code performs exactly that for $N = 50$:
   </p>
 
   <CodeBlock
@@ -313,7 +311,7 @@
   />
 
   <p>
-    <FigureLink id="dog-color-compression" capitalized /> shows the resulting
+    <FigureLink id="dog-color-compression" capitalized /> displays the resulting
     images represented by $I_c$ for several values of $N$ (click
     <a :href="dogColorCompressionMovie">here</a> to see an animation showing all
     images obtained for values of $N$ ranging from $50$ to $1$):
@@ -322,8 +320,9 @@
   <FigureBlock
     id="dog-color-compression"
     caption="Images generated by compressing $I$ with different values of $N$.
-             Figures (a), (b), (c), (d), (e) and (f) show the resulting images
-             for $N = 200$, $100$, $50$, $25$, $10$ and $5$ respectively."
+             Figures (a), (b), (c), (d), (e), and (f) display the resulting
+             images for $N = 200$, $100$, $50$, $25$, $10$, and $5$
+             respectively."
   >
     <BaseSubfigure>
       <ResponsiveImage
@@ -370,11 +369,11 @@
   </FigureBlock>
 
   <p>
-    Similarly to what we did earlier, we can compare the sizes of PNG images
-    generated from $I_c$ for multiple values of $N$ with the size of the
-    original PNG image (<FigureLink id="dog-color" />). Since the latter is
-    already locally stored as a file, all we need to do is store $I_c$ as a PNG
-    image and check its size for a fixed value of $N$:
+    In the same vein as earlier, we can compare the sizes of PNG images created
+    from $I_c$ for various values of $N$ to the size of the original PNG image
+    (<FigureLink id="dog-color" />). Since the latter is already stored locally
+    as a file, all we need to do is save $I_c$ as a PNG image and check its size
+    for a given value of $N$:
   </p>
 
   <CodeBlock language="matlab" code='imwrite(Ic, "dog-color-compressed.png")' />
@@ -389,8 +388,8 @@
     id="image-sizes-color"
     :columns="2"
     caption="Size of generated PNG images for different values
-             of $N$, with $N = 400$ corresponding to the original image
-             since all singular values are kept during the compression process."
+             of $N$, with $N = 400$ representing the original image
+             as all singular values are retained during the compression process."
   >
     <span>$N$</span>
     <b>Size $(\KilobyteSlim)$</b>
@@ -418,18 +417,19 @@
   </TableBlock>
 
   <p>
-    As we can see from the numbers above, the PNG images become smaller as we
-    discard more singular values from $I_{ij1}$, $I_{ij2}$ and $I_{ij3}$, but
-    again only slowly as $N$ is decreased. Even for $N = 5$, the resulting image
-    is only $45\%$ smaller than the original one.
+    From the numbers above, it's clear that the PNG images shrink in size as we
+    discard more singular values from $I_{ij1}$, $I_{ij2}$, and $I_{ij3}$.
+    However, the reduction in size again happens quite slowly as we decrease
+    $N$. Even for $N = 5$, the resulting image is only $45\%$ smaller than the
+    original.
   </p>
 
   <p>
-    Out of curiosity, I also tried generating images in JPEG format instead of
-    PNG. The resulting images were actually larger than the original one except
-    for small values of $N$, an interesting result indicating that mixing
-    compression algorithms can yield worse results compared to using a single
-    one.
+    Just out of curiosity, I also experimented with generating images in the
+    JPEG format instead of PNG. Interestingly, the resulting images were
+    actually larger than the original one, except for small values of $N$. This
+    unexpected outcome suggests that combining compression algorithms can
+    sometimes lead to worse results compared to using a single one.
   </p>
 </template>
 
