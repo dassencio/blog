7ae6d23dbbdaff56e53ceedcf17ea57f1c6a304f
Diego Assencio <diego@assencio.com>
2023-05-24 22:18:45 +0200

    Add script to create new post

---
+++ new-post.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+post_id=$1
+post_file="./src/posts/${post_id}.vue"
+
+if [ -z "$post_id" ]; then
+    echo "Usage: $0 <post-id>"
+    exit 1
+fi
+
+if [ -f "$post_file" ]; then
+    echo "File $post_file already exists."
+    exit 1
+fi
+
+# Create a file "$post_file"
+cat > "$post_file" <<EOF
+<template>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { onViewMounted } from "@/functions";
+
+onMounted(() => onViewMounted());
+</script>
+EOF
