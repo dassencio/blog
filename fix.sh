2954ce859fad2f311ffdae108f62503cac851795
Diego Assencio <diego@assencio.com>
2023-05-24 22:12:27 +0200

    Implement multi-line content support in migration script

--- fix.sh
+++ fix.sh
@@ -1,56 +1,77 @@
 #!/usr/bin/env bash
 
-# set -ex
 set -e
 
-for i in {1..9}
-do
-  # Fix titles.
-  gsed -i 's/<h3>\([^<]*\)<\/h3>/<SectionTitle>\1<\/SectionTitle>/g' $1
+# Fix titles.
+gsed -i 's/<h3>\([^<]*\)<\/h3>/<SectionTitle>\1<\/SectionTitle>/g' $1
 
-  # Fix monospace text.
-  gsed -i 's/<span class="msnw">\([^<]*\)<\/span>/<code>\1<\/code>/g' $1
-  gsed -i "s/<span class='msnw'>\([^<]*\)<\/span>/<code>\1<\/code>/g" $1
+# Fix italic text.
+awk '
+BEGIN {
+    RS="<span class=(\"italic\"|'\''italic'\'')>|</span>"; ORS="";
+}
+NR%2==0 {
+    print "<i>" $0 "</i>";
+}
+NR%2!=0 {
+    print $0;
+}
+' "$1" > "__tmpfile__" && mv "__tmpfile__" "$1"
 
-  # Fix italic text.
-  gsed -i 's/<span class="italic">\([^<]*\)<\/span>/<i>\1<\/i>/g' $1
+# Fix monospace text.
+awk '
+BEGIN {
+    RS="<span class=(\"msnw\"|'\''msnw'\'')>|</span>"; ORS="";
+}
+NR%2==0 {
+    print "<code>" $0 "</code>";
+}
+NR%2!=0 {
+    print $0;
+}
+' "$1" > "__tmpfile__" && mv "__tmpfile__" "$1"
 
-  # Fix bold text.
-  gsed -i 's/<span class="bold">\([^<]*\)<\/span>/<b>\1<\/b>/g' $1
+# Fix equations.
+awk '
+BEGIN {
+    RS="\\$\\$"; ORS="";
+}
+NR%2==0 {
+    print "</p>\n\n<EquationBlock>" $0 "</EquationBlock>\n\n<p>";
+}
+NR%2!=0 {
+    print $0;
+}
+' "$1" > "__tmpfile__" && mv "__tmpfile__" "$1"
 
-  # Fix equations.
-  for i in 1 .. 999
-  do
-    gsed -i '0,/\$\$/s//<\/p>\n\n<EquationBlock>\n/' $1
-    gsed -i '0,/\$\$/s//<\/EquationBlock>\n\n<p>\n/' $1
-  done
-  gsed -i 's/\displaystyle//g' $1
-  gsed -i 's/&amp;=&amp;/\&=/g' $1
-  gsed -i 's/\\nonumber\\\\\[5pt]/\\\\/g' $1
-  gsed -i 's/%INDEX%_//g' $1
-  gsed -i 's/\\times/\\cdot/g' $1
+# Fix equation contents.
+gsed -i 's/\displaystyle//g' $1
+gsed -i 's/&amp;=&amp;/\&=/g' $1
+gsed -i 's/\\nonumber\\\\\[5pt]/\\\\/g' $1
+gsed -i 's/%INDEX%_//g' $1
+gsed -i 's/\\times/\\cdot/g' $1
 
-  # Fix code (partially).
-  gsed -i 's/<div class="code"><pre>/<CodeBlock code="/g' $1
-  gsed -i "s/<div class='code'><pre>/<CodeBlock code=\"/g" $1
-  gsed -i 's/<\/div><\/pre>/  " \/>/g' $1
-  gsed -i 's/<div class="code">/<CodeBlock code="/g' $1
-  gsed -i "s/<div class='code'>/<CodeBlock code=\"/g" $1
-  gsed -i 's/<pre>//g' $1
-  gsed -i 's/<\/pre>//g' $1
-  gsed -i 's/<span class="highlight-code">\([^<]*\)<\/span>/**\1**/g' $1
-  gsed -i "s/<span class='highlight-code'>\([^<]*\)<\/span>/**\1**/g" $1
+# Fix code (partially).
+awk '
+BEGIN {
+  RS="</div>"
+  ORS=""
+}
+{
+  gsub(/<div class=['"'"'"]code['"'"'"]><pre>/, "<CodeBlock code=\"")
+  gsub(/<\/pre>/, "\" \/>")
+  print
+}
+' "$1" > "__tmpfile__" && mv "__tmpfile__" "$1"
 
-  # Fix figure links.
-  for i in {1..99}
-  do
-    gsed -i "s/<a href=\"#fig_$i\">figure $i<\/a>/<FigureLink :id=\"CHANGE-$i\" \/>/g" $1
-    gsed -i "s/<a href=\"#fig_$i\">Figure $i<\/a>/<FigureLink :id=\"CHANGE-$i\" \/>/g" $1
-  done
+gsed -i "/<div class=['\"]code-after-code['\"]>/d" $1
 
-  # Replace tabs with spaces.
-  gsed -i 's/\t/  /g' $1
+# Fix figure links.
+gsed -i "s/<a href=\"#fig_$i\">figure $i<\/a>/<FigureLink :id=\"CHANGE-$i\" \/>/g" $1
+gsed -i "s/<a href=\"#fig_$i\">Figure $i<\/a>/<FigureLink :id=\"CHANGE-$i\" \/>/g" $1
 
-  # Other fixes.
-  gsed -i "s/you are/you're/g" $1
-done
+# Replace tabs with spaces.
+gsed -i 's/\t/  /g' $1
+
+# Other fixes.
+gsed -i "s/you are/you're/g" $1
