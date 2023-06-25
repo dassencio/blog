756207d9c01a94101b4d4a397f20717a007234e8
Diego Assencio <diego@assencio.com>
2023-06-25 15:08:30 +0200

    Improve post text

--- src/posts/24.vue
+++ src/posts/24.vue
@@ -2,39 +2,37 @@
   <p>
     During my university studies, a
     <a href="https://pt.wikipedia.org/wiki/Henrique_Fleming">professor</a>
-    asked us a very interesting question which will be the subject of this post:
-    <i
-      >What is the probability of finding the electron of a Hydrogen atom inside
-      the atom's nucleus?</i
-    >
+    posed an intriguing question, which I will be discussing in this post: what
+    is the probability of finding the electron of a Hydrogen atom inside the
+    atom's nucleus?
   </p>
 
   <p>
     The
     <a href="https://en.wikipedia.org/wiki/Charge_radius">radius of a proton</a>
-    (the nucleus of a Hydrogen atom) is $r_0 \approx 10^{-15}\Meter$. The answer
-    to the question will be hard to find if one tries to compute it analytically
-    by integrating the probability density function $|\psi(\Vec{x})|^2$ over the
-    volume of the proton:
+    (which makes up the nucleus of a Hydrogen atom) is approximately $r_0 =
+    10^{-15} \Meter$. Finding an answer to the question analytically by
+    integrating the probability density function $|\psi(\Vec{x})|^2$ over the
+    volume of the proton proves to be a complex task:
   </p>
 
   <EquationBlock>
     P = \int_{\|\Vec{x}\| \leq r_0} |\psi(\Vec{x})|^2 \D{\Vec{x}}
-    \label{P-exact}
   </EquationBlock>
 
   <p>
-    where $\|\Vec{x}\| \leq r_0$ refers to the fact that the integral should be
-    computed over a sphere of radius $r_0$ centered at the origin (where the
-    center of the proton is assumed to be). Instead, as my professor told us, a
-    much better idea is to compute $P$ "<i
+    where $\|\Vec{x}\| \leq r_0$ indicates that the integral should be computed
+    over a sphere of radius $r_0$ centered at the origin, where the center of
+    the proton is assumed to be located. However, my professor offered an
+    ingenious alternative. He suggested it would be more practical to compute
+    $P$ "<i
       >as
       <a href="https://en.wikipedia.org/wiki/Enrico_Fermi">Fermi</a> would</i
-    >". By that, he meant the problem can be solved much more easily by using a
-    suitable
+    >". This meant that the problem could be simplified by using a suitable
     <a href="https://en.wikipedia.org/wiki/Fermi_problem">approximation</a>,
-    namely that $|\psi(\Vec{x})|$ is constant over the extension of the nucleus.
-    This makes the computation of $P$ a much less daunting task:
+    specifically by assuming that $|\psi(\Vec{x})|$ remains constant over the
+    extent of the nucleus. This assumption makes the computation of $P$ much
+    more manageable:
   </p>
 
   <EquationBlock>
@@ -44,9 +42,9 @@
 
   <p>
     For any state with a known wave function $\psi(\Vec{x})$, we can use
-    equation \eqref{P-approximate} to obtain a very good approximation of $P$.
-    Consider, for instance, the eigenfunctions of the Hydrogen atom. They are
-    specified by three quantum numbers $(n, l, m)$ and always have the form:
+    equation \eqref{P-approximate} to obtain a highly accurate approximation of
+    $P$. Take, for instance, the eigenfunctions of the Hydrogen atom. These are
+    specified by three quantum numbers $(n, l, m)$ and have the form:
   </p>
 
   <EquationBlock>
@@ -58,8 +56,8 @@
     <a href="https://en.wikipedia.org/wiki/Spherical_harmonic"
       >normalized spherical harmonic</a
     >
-    of degree $l$ and order $m$ and $R_{nl}(r)$ is a real-valued function of $r$
-    which has the form (see
+    of degree $l$ and order $m$, and $R_{nl}(r)$ is a real-valued function of
+    $r$ that takes the following form (see
     <ReferenceLink id="griffiths-quantum-mechanics" />, equations 4.73 - 4.75):
   </p>
 
@@ -68,7 +66,7 @@
     \label{radial-wave-function}
   </EquationBlock>
 
-  <p>where $\rho = r / (a_0 n)$, $v(\rho)$ is a polynomial function and</p>
+  <p>Above, $\rho = r / (a_0 n)$, $v(\rho)$ is a polynomial function, and:</p>
 
   <EquationBlock>
     a_0 = \frac{4\pi \epsilon_0 \hbar^2}{me^2} = 0.529 \cdot 10^{-10}\Meter
@@ -76,10 +74,11 @@
 
   <p>
     is the
-    <a href="https://en.wikipedia.org/wiki/Bohr_radius">Bohr radius</a> ($m$ is
-    the mass of the electron). From equation \eqref{radial-wave-function} and
-    the definition of $\rho$, we have that $R_{nl}(r) \propto r^l e^{-\rho}
-    v(\rho)$ and therefore $R_{nl}(0) = 0$ for all $l \gt 0$. Hence:
+    <a href="https://en.wikipedia.org/wiki/Bohr_radius">Bohr radius</a>, where
+    $m$ denotes the mass of the electron. From equation
+    \eqref{radial-wave-function} and the definition of $\rho$, it follows that
+    $R_{nl}(r) \propto r^l e^{-\rho} v(\rho)$ and therefore $R_{nl}(0) = 0$ for
+    all $l \gt 0$. Hence:
   </p>
 
   <EquationBlock>
@@ -88,19 +87,19 @@
   </EquationBlock>
 
   <p>
-    whenever $l \gt 0$. Denoting the value of $P$ for the eigenstate specified
-    by the quantum numbers $(n, l, m)$ as $P_{nlm}$, equation
-    \eqref{P-approximate} then gives us:
+    whenever $l \gt 0$. Let's denote the value of $P$ for the eigenstate
+    specified by the quantum numbers $(n, l, m)$ as $P_{nlm}$. Using equation
+    \eqref{P-approximate}, we then obtain:
   </p>
 
   <EquationBlock> l \gt 0 \Longrightarrow P_{nlm} = 0 </EquationBlock>
 
   <p>
-    For the eigenstates of the Hydrogen atom, the probability of finding the
-    electron inside the nucleus can therefore be nonzero only when $l = 0$.
-    Since $m = 0$ is the only allowed value of $m$ in this case (remember that
-    $m$ can only take integer values between $-l$ and $l$), the wave functions
-    we need to consider will have the form (see
+    Thus, for the eigenstates of the Hydrogen atom, the probability of finding
+    the electron inside the nucleus can be nonzero only when $l = 0$. Since the
+    only allowed value of $m$ in this case is $m = 0$ (recall that $m$ can only
+    take integer values between $-l$ and $l$), the wave functions we need to
+    consider take the form (see
     <ReferenceLink id="griffiths-quantum-mechanics" />, table 4.2):
   </p>
 
@@ -110,8 +109,9 @@
   </EquationBlock>
 
   <p>
-    The equations below give us $\psi_{n00}(r, \theta, \phi)$ for $n = 1, 2, 3$
-    (see <ReferenceLink id="griffiths-quantum-mechanics" />, table 4.6):
+    The equations below provide us with $\psi_{n00}(r, \theta, \phi)$ for $n =
+    1, 2, 3$ (see <ReferenceLink id="griffiths-quantum-mechanics" />, table
+    4.6):
   </p>
 
   <!-- prettier-ignore -->
@@ -125,7 +125,7 @@
       \frac{2}{27}\left(\frac{r}{a_0}\right)^2\right]e^{-r/3a_0}
   </EquationBlock>
 
-  <p>For $r = 0$, we obtain:</p>
+  <p>At $r = 0$, we have:</p>
 
   <EquationBlock align>
     \psi_{100}(0,\theta,\phi) &= \frac{1}{\sqrt{\pi a_0^3}} \\
@@ -134,7 +134,7 @@
   </EquationBlock>
 
   <p>
-    These values can now be used in equation \eqref{P-exact} to obtain the
+    We can now use these values in equation \eqref{P-approximate} to compute the
     probability of finding the electron inside the nucleus for the eigenstates
     with $n = 1, 2, 3$ and $l = m = 0$:
   </p>
@@ -151,7 +151,7 @@
 
   <p>
     The probability of finding the electron inside the nucleus of a Hydrogen
-    atom is therefore extremely small, and decreases as the principal quantum
+    atom is therefore exceedingly small and decreases as the principal quantum
     number $n$ increases.
   </p>
 
