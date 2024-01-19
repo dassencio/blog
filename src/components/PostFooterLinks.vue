628c077153d1dba422c4edcf42d621cece5d231e
Diego Assencio <diego@assencio.com>
2024-01-19 22:18:17 +0100

    Add footer links to posts

---
+++ src/components/PostFooterLinks.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="links">
+    <a class="links__item" :href="issueTypeToUrl('question')">Ask a question</a>
+    <a class="links__item" :href="issueTypeToUrl('error')">Report an error</a>
+    <a class="links__item" :href="postSourceUrl">Source code</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Post } from "@/posts";
+
+const props = defineProps<{
+  post: Post;
+}>();
+
+const blogUser = "dassencio";
+const blogRepo = "blog";
+const blogUrl = `https://github.com/${blogUser}/${blogRepo}`;
+
+const postSourceUrl = `${blogUrl}/tree/main/src/posts/${props.post.id}.vue`;
+
+// Body of GitHub issue of type "error".
+const errorIssueBody = `
+**Post URL**\n
+${window.location.href}\n
+**Description**\n
+Please describe the error here.
+`;
+
+// Body of GitHub issue of type "question".
+const questionIssueBody = `
+**Post URL**\n
+${window.location.href}\n
+**Question**\n
+Please enter your question here.
+`;
+
+// GitHub issue labels.
+const issueLabel = {
+  error: "Error",
+  question: "Question",
+};
+
+// GitHub issue bodies.
+const issueBody = {
+  error: errorIssueBody,
+  question: questionIssueBody,
+};
+
+function issueTypeToUrl(issueType: keyof typeof issueLabel) {
+  const body = encodeURIComponent(issueBody[issueType].trim());
+  const label = issueLabel[issueType];
+  return `${blogUrl}/issues/new?labels=${label}&assignees=${blogUser}&body=${body}`;
+}
+</script>
+
+<style scoped lang="scss">
+.links {
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: $post-footer-links-vertical-gap $post-footer-links-horizontal-gap;
+  justify-content: center;
+  margin: $post-footer-links-vertical-margin 0;
+  &__item {
+    font-size: $post-footer-link-font-size;
+    line-height: fontSizeToLineHeight($post-footer-link-font-size);
+    text-align: center;
+    text-wrap: nowrap;
+  }
+}
+</style>
---
+++ src/components/PostFooterSeparator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="separator">
+    <div class="separator__circle" v-for="i in 3" :key="i"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.separator {
+  align-items: center;
+  column-gap: $post-footer-separator-horizontal-gap;
+  display: flex;
+  justify-content: center;
+  margin: $post-footer-separator-vertical-margin 0;
+  &__circle {
+    background-color: $post-footer-separator-circle-color;
+    border-radius: 50%;
+    height: 2 * $post-footer-separator-circle-radius;
+    width: 2 * $post-footer-separator-circle-radius;
+  }
+}
+</style>
