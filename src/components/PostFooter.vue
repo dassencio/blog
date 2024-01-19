b5d0cb5f94dc7420a911bde536cd151ac563a9e6
Diego Assencio <diego@assencio.com>
2024-01-19 22:18:17 +0100

    Add footer links to posts

---
+++ src/components/PostFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <PostFooterSeparator />
+    <PostFooterLinks :post="post" />
+  </footer>
+</template>
+
+<script setup lang="ts">
+import PostFooterLinks from "@/components/PostFooterLinks.vue";
+import PostFooterSeparator from "@/components/PostFooterSeparator.vue";
+import type { Post } from "@/posts";
+
+defineProps<{
+  post: Post;
+}>();
+</script>
